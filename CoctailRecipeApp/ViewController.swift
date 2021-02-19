c33a844a39348f9f1015cf43f2cd3980fe6e46cc
PageKallop <PageMarie78@gmail.com>
2021-02-19 11:01:47 -0600

    First commit Create gradient

--- CoctailRecipeApp/ViewController.swift
+++ CoctailRecipeApp/ViewController.swift
@@ -8,9 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let containerView: UIView = {
+        let screenSize: CGRect = UIScreen.main.bounds
+        let vc = UIView(frame: CGRect(x: 0, y: 0, width: screenSize.width, height: screenSize.height))
+        let gradient = CAGradientLayer()
+        gradient.frame = vc.bounds
+        gradient.colors = [UIColor.white.cgColor, UIColor.black.cgColor]
+        vc.layer.insertSublayer(gradient, at: 0)
+        return vc
+        
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view = containerView
         // Do any additional setup after loading the view.
     }
 
