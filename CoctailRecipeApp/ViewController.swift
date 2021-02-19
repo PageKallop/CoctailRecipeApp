37873b78accb26d673fe6808fafdd5f72e3e6eef
PageKallop <PageMarie78@gmail.com>
2021-02-19 14:12:24 -0600

    Adding searchBar

--- CoctailRecipeApp/ViewController.swift
+++ CoctailRecipeApp/ViewController.swift
@@ -7,24 +7,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController  {
     
-    let containerView: UIView = {
-        let screenSize: CGRect = UIScreen.main.bounds
-        let vc = UIView(frame: CGRect(x: 0, y: 0, width: screenSize.width, height: screenSize.height))
-        let gradient = CAGradientLayer()
-        gradient.frame = vc.bounds
-        gradient.colors = [UIColor.white.cgColor, UIColor.black.cgColor]
-        vc.layer.insertSublayer(gradient, at: 0)
-        return vc
-        
+    let searchBar : UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.searchBarStyle = UISearchBar.Style.prominent
+        searchBar.placeholder = "Search Coctails..."
+//        searchBar.sizeToFit()
+        searchBar.isTranslucent = false
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        return searchBar
     }()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
+   
+        view.backgroundColor = UIColor(named: "lightBlue")
+        topLayout()
+       
+    }
+    
+    
+    func topLayout() {
+       
+        let containerView = UIView()
+        view.addSubview(containerView)
+        containerView.backgroundColor = .white
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+//
+//        let gradient = CAGradientLayer()
+//        gradient.frame = containerView.bounds
+//        gradient.colors = [UIColor(named: "lightBlue")?.cgColor ?? UIColor.systemTeal.cgColor, UIColor.white.cgColor]
+//        containerView.layer.insertSublayer(gradient, at: 1)
+//        gradient.locations = [0.7, 0.75]
+        containerView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        containerView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        containerView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        containerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5).isActive = true
         
-        view = containerView
-        // Do any additional setup after loading the view.
+        containerView.addSubview(searchBar)
+        
+        searchBar.centerXAnchor.constraint(equalTo: containerView.centerXAnchor).isActive = true
+        searchBar.centerYAnchor.constraint(equalTo: containerView.centerYAnchor).isActive = true
+        searchBar.heightAnchor.constraint(equalToConstant: 200).isActive = true
+
     }
 
 
