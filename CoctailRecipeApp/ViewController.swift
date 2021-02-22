74402d183e7af4281fed1146ae4ee664f704c99b
PageKallop <PageMarie78@gmail.com>
2021-02-22 14:33:56 -0600

    Create scroll view

--- CoctailRecipeApp/ViewController.swift
+++ CoctailRecipeApp/ViewController.swift
@@ -8,13 +8,25 @@
 import UIKit
 
 
-class ViewController: UIViewController  {
+class ViewController: UIViewController, UIScrollViewDelegate {
+    
+    
+    
+    let stackView = UIStackView()
+    
+    let scrollView : UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.contentSize = CGSize(width: 200, height: 200)
+        scrollView.isScrollEnabled = true
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
     
     
     
     let searchTextField : UITextField = {
       let searchTextField = UITextField()
-        searchTextField.placeholder = "Find Your Coctail..."
+        searchTextField.placeholder = "Coctail?"
         searchTextField.borderStyle = .roundedRect
         searchTextField.translatesAutoresizingMaskIntoConstraints = false
         let paddingView: UIView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 20))
@@ -29,8 +41,8 @@ class ViewController: UIViewController  {
     let findButton : UIButton = {
         let findButton = UIButton(frame: CGRect(x: 100, y: 100, width: 130, height: 75))
         findButton.backgroundColor = UIColor(red: 240/255, green: 230/255, blue: 140/255, alpha: 0.5)
-        findButton.setTitle("Mixologist", for: .normal)
-        findButton.titleLabel?.font = UIFont(name: "Thonburi-Bold", size: 15)
+        findButton.setTitle("Mixologist...", for: .normal)
+        findButton.titleLabel?.font = UIFont(name: "SnellRoundhand", size: 25)
         findButton.setTitleColor(UIColor.black, for: .normal)
         findButton.layer.cornerRadius = 30
         findButton.addTarget(self, action: #selector(findButtonAction(sender:)), for: .touchUpInside)
@@ -60,11 +72,28 @@ class ViewController: UIViewController  {
         let containerView = UIView()
         view.addSubview(containerView)
         containerView.translatesAutoresizingMaskIntoConstraints = false
- 
+        containerView.backgroundColor = .purple
         containerView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
         containerView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         containerView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         containerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5).isActive = true
+        
+        containerView.addSubview(scrollView)
+        scrollView.backgroundColor = UIColor.cyan
+        scrollView.topAnchor.constraint(equalTo: containerView.safeAreaLayoutGuide.topAnchor).isActive = true
+        scrollView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor).isActive = true
+        scrollView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: containerView.centerYAnchor).isActive = true
+        
+//        scrollView.addSubview(stackView)
+//        stackView.translatesAutoresizingMaskIntoConstraints = false
+//        stackView.axis = .vertical
+//        stackView.spacing = 10
+//        stackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor).isActive = true
+//        stackView.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
+//        stackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor).isActive = true
+//        stackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
+//        stackView.backgroundColor = .white
 
     }
     
@@ -79,7 +108,7 @@ class ViewController: UIViewController  {
         
         
         bottomContainerView.addSubview(searchTextField)
-        searchTextField.topAnchor.constraint(equalTo: bottomContainerView.topAnchor, constant: 100).isActive = true
+        searchTextField.topAnchor.constraint(equalTo: bottomContainerView.topAnchor, constant: 120).isActive = true
         searchTextField.leadingAnchor.constraint(equalTo: bottomContainerView.leadingAnchor,constant: 15).isActive = true
         searchTextField.bottomAnchor.constraint(equalTo: bottomContainerView.bottomAnchor,constant: -300).isActive = true
         searchTextField.trailingAnchor.constraint(equalTo: bottomContainerView.trailingAnchor, constant: -100).isActive = true
